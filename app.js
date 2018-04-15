75b4d445a2fc856796e0ce6045f4331d386bae73
malforsaja <malfor.saja@gmail.com>
2018-04-15 23:24:51 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mysql = require('mysql');
+
+// Create connection
+const db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '123456',
+    database : 'nodemysql'
+});
+
+// Connect
+db.connect((err) => {
+    if(err){
+        throw err;
+    }
+    console.log('MySQL Connected...');
+});
+
+const app = express();
+
+app.listen('3000', () => {
+    console.log('Server started on port 3000');
+});
