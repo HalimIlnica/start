f27498735f4e371faa1ea6ba314a356129e438ec
malforsaja <malfor.saja@gmail.com>
2018-04-16 23:20:10 +0200

    localhost

---
+++ README.md
@@ -0,0 +1,14 @@
+# start project
+
+## install dependencies
+1. clone the project
+
+2. ```npm install```
+
+___
+
+## start project
+To start the project use the command: **node app.js** or **node app**
+
+## open into the browser
+localhost:3000
--- app.js
+++ app.js
@@ -1,12 +1,17 @@
+const path = require('path');
 const express = require('express');
 const mysql = require('mysql');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'client')));
 
 // Create connection
 const db = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
-    password : '123456',
-    database : 'nodemysql'
+    password : '',
+    database : 'startdb'
 });
 
 // Connect
@@ -16,8 +21,6 @@ db.connect((err) => {
     }
     console.log('MySQL Connected...');
 });
-
-const app = express();
 
 app.listen('3000', () => {
     console.log('Server started on port 3000');
